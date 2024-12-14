df6c1ac64a2c609eba22be9ab84b51d5972e7242
CarlinoCalogero <calogerocarlino19@gmail.com>
2024-12-14 16:15:59 +0100

    now generic sensor values above threshold generation have 0.01 probability of being generated

--- project/publisher/app/sensors/generic_sensor.py
+++ project/publisher/app/sensors/generic_sensor.py
@@ -7,7 +7,7 @@ class GenericSensor:
         self.min_threshold, self.max_threshold = thresholds
         
     def read(self):
-        if random.random() < 0.9: # generate random below threshold with probability 0.9
+        if random.random() < 0.99: # generate random below threshold with probability 0.9
             return random.uniform(self.min_value, self.max_value)
         # generate random above threshold with probability 0.1
         return random.uniform(self.min_threshold, self.max_threshold)
